305aeb8fff34c4094bc9ebeab158ce5594f36a19
Cajunbbbb <vlulab_240000000740@uic.edu.ph>
2025-03-04 11:32:48 +0800

    Add files via upload

---
+++ lulab_at4/Lulab_AT4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lulab_at4;
+
+/**
+ *
+ * @author ComLab204_09
+ */
+public class Lulab_AT4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
